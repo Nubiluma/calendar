8efefa638f7bc2b39fc037be2592a1e89f7a4bc9
Sandra Petereit <sandrap3393@googlemail.com>
2023-02-04 21:16:41 +0100

    #4 disable CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const Appointment = require("./appointment");
 const db = require("./db");
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json({ extended: true, limit: "1mb" }));
 
 app.get("/", (_, res) => {
